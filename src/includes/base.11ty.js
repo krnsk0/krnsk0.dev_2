e7bdcd3400b5448ef5062d9d6197e8a80a21cefe
krnsk0 <jonathan.kurinsky@ingka.ikea.com>
2020-09-19 21:27:08 -0400

    set up basic stylesheet

--- src/includes/base.11ty.js
+++ src/includes/base.11ty.js
@@ -10,13 +10,16 @@ const navbar = (url) => {
   };
 
   return `
-<nav>
+<nav class="nav--outer">
+  <div class="nav--title">KRNSK0</div>
+  <div class="nav--inner">
   ${Object.entries(links)
     .map(([path, text]) => {
       const classString = url.startsWith(path) ? 'nav--link-active' : '';
       return `<a href="${path}" class="nav--link ${classString}">${text}</a>`;
     })
     .join('')}
+  </div>
 </nav>
   `;
 };
@@ -28,6 +31,7 @@ module.exports.render = ({ title, content, entry, page: { url } }) => {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@200&family=Roboto+Mono&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="/css/reset.css" type="text/css">
     <link rel="stylesheet" href="/css/styles.css" type="text/css">
     <title>${title}</title>
